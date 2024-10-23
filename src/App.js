0ccef619da6356bde7f2cf81d0f3172d2f0a5235
NurulIslam17 <nurulcse09@gmail.com>
2024-10-23 22:41:17 +0600

    ok

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./page/Home";
 import Login from "./page/Login";
 import NotFound from "./page/NotFound";
 import Register from "./page/Register";
+import Dashboard from "./page/Admin/Dashboard";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
