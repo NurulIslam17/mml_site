cc30d8970fbce2a7143c8c91461884ad9591a974
NurulIslam17 <nurulcse09@gmail.com>
2024-11-05 21:17:33 +0600

    Ok

--- src/components/global/Modal.jsx
+++ src/components/global/Modal.jsx
@@ -24,7 +24,7 @@ const Modal = ({ title, modalTitle, action,modalBody, customClass }) => {
       {showModal ? (
         <>
           <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-            <div className="bg-green-500  relative w-auto my-6 mx-auto max-w-3xl">
+            <div className="relative w-auto my-6 mx-auto max-w-7xl">
               {/*content*/}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {/*header*/}
@@ -42,7 +42,7 @@ const Modal = ({ title, modalTitle, action,modalBody, customClass }) => {
                   </button>
                 </div>
                 {/*body*/}
-                <div className="relative p-6 flex-auto">
+                <div className="relative p-6 flex-aut">
                 {modalBody}
                 </div>
                 {/*footer*/}
